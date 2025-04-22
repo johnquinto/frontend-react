f4247358b23c7b19954a7838573a74c4c0355c13
John Quinto <joaoquinto1010@gmail.com>
2025-04-22 13:17:07 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,9 +9,8 @@ export default defineConfig({
     port: 3000, // Para desenvolvimento local
   },
   preview: {
-    port: 4173, // Para o Render
+    port: 4173, // Porta para o Render
     host: '0.0.0.0', // Expor a aplicação para qualquer IP
+    allowedHosts: ['frontend-react-w8wb.onrender.com'], // Adiciona o domínio do Render
   },
 });
-
-
